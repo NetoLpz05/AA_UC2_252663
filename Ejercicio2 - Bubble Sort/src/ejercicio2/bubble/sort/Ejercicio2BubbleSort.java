927394a160f65114fb224c6222e4b1dab9dfc4bb
NetoLpz05 <jesus.lopez252663@potros.itson.edu.mx>
2025-03-04 20:00:31 -0700

    funcion para imprimir el arreglo

--- Ejercicio2 - Bubble Sort/src/ejercicio2/bubble/sort/Ejercicio2BubbleSort.java
+++ Ejercicio2 - Bubble Sort/src/ejercicio2/bubble/sort/Ejercicio2BubbleSort.java
@@ -10,15 +10,27 @@ public class Ejercicio2BubbleSort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] a = {8, 15, 30};
+        int[] a = {8, 30, 15};
         
         System.out.println("Arreglo original:");
-        System.out.println(a);
+        imprimirArreglo(a);
         
         Ordenamientos ordenamiento = new Ordenamientos(a);
         ordenamiento.burbuja();
         
         System.out.println("Arreglo ordenado:");
-        System.out.println(a);
+        imprimirArreglo(a);
+    }
+    
+    // Función para imprimir el arreglo
+    public static void imprimirArreglo(int[] a) {
+        System.out.print("[");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+            if (i < a.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
