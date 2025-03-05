01ee0ef08958fea70dc08eebc5ec829c9c084ae8
NetoLpz05 <jesus.lopez252663@potros.itson.edu.mx>
2025-03-04 19:41:57 -0700

    Creacion del algoritmo de burbuja

---
+++ Ejercicio2 - Bubble Sort/src/ejercicio2/bubble/sort/Ordenamientos.java
@@ -0,0 +1,27 @@
+package ejercicio2.bubble.sort;
+
+/**
+ * Clase que implementa el algoritmo de ordenamiento Bubble Sort.
+ * @author ErnestoLpz_252663
+ */
+public class Ordenamientos {
+    int a[];
+    
+    public Ordenamientos(int[] a){
+        this.a = a;
+    }
+    
+    public void burbuja(){
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a.length - i; i++) {
+                //Condicion
+                if(a[j]>a[j+1]){
+                    //Intercambio
+                    int aux = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = aux;
+                }
+            }
+        }
+    }
+}
